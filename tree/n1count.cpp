6f5560e4445f79851d2434cc265885b500ca0dca
amitsingh-bit <amitsinghme80521@gmail.com>
2025-11-25 15:21:48 +0530

    count of nodes having 1 childs

---
+++ tree/n1count.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+/******************************************/
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+};
+
+/******************************************/
+
+node *makeNode(int x){
+    node *newNode=(node*)malloc(sizeof(node));
+    newNode->data=x;
+    newNode->left=NULL;
+    newNode->right=NULL;
+    return newNode;
+}
+
+/******************************************/
+
+struct Tree{
+    node *root;
+};
+
+
+/******************************************/
+
+void CreateTree(node **t){
+    int choice;
+    cout<<"Enter the choice if left of "<<(*t)->data<<" exists(1/0): ";
+    cin>>choice;
+    if(choice==1){
+        int x;
+        cout<<"Enter the value to be inserted at left of "<<(*t)->data<<": ";
+        cin>>x;
+        node *p=makeNode(x);
+        (*t)->left=p;
+        CreateTree(&p);
+    }
+    int y;
+    cout<<"Enter the choice if right of "<<(*t)->data<<" exists(1/0): ";
+    cin>>y;
+    if(y==1){
+        int a;
+        cout<<"Enter the value to be inserted at right of "<<(*t)->data<<": ";
+        cin>>a;
+        node *p=makeNode(a);
+        (*t)->right=p;
+        CreateTree(&p);
+    }
+}
+
+/******************************************/
+
+
+int CountN1(node *t) {
+    if (t == NULL) {
+        return 0;
+    }
+    else if (t->left == NULL && t->right == NULL) {
+        return 0;
+    }
+    else if (t->left != NULL && t->right != NULL) {
+        return  CountN1(t->left) + CountN1(t->right);
+    }
+    else {
+        return 1+CountN1(t->left) + CountN1(t->right);
+    }
+}
+
+/***************************************************************/
+
+int main() {
+
+    Tree t;
+
+    int rootValue;
+    cout << "Enter the value of root node: ";
+    cin >> rootValue;
+
+    t.root = makeNode(rootValue);
+
+    cout << "\n--- Start Building Your Tree ---\n";
+    CreateTree(&t.root);
+
+    cout << "\nTree created successfully!\n";
+
+    cout << "Total number of nodes having 1 childs = " << CountN1(t.root) << endl;
+
+    return 0;
+}
