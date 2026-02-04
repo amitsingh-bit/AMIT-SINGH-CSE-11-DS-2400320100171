5a8ed4f519fa2f62ba19290ded92af349e27b960
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-04 10:52:51 +0530

    vector

---
+++ vector/display.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    v.push_back(60);
+    v.push_back(70);
+    cout<<"the vector elements are:"<<endl;
+    int i=0;
+    for(i=0;i<=6;i++){
+        cout<<v[i]<<endl;
+    }
+
+}
